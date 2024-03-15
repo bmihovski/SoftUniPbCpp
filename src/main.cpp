9ac1e83e2bcff09e0b65821c1576fe49903046a0
Boyan Mihovski <Boyan.Ma.Mihovski@bmw.de>
2024-03-15 22:28:01 +0200

    add print name of numbers

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,64 @@
 #include <iostream>
 #include <climits>
+#include <iomanip>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <list>
+#include <stack>
+#include <queue>
 
 #ifdef TESTING
 #include <gtest/gtest.h>
 #endif
 
+const std::vector<std::string> singleDigits = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+const std::vector<std::string> tensNumber = {"ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty",
+                                             "ninety"};
+const std::vector<std::string> toNineteen = {"eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen",
+                                             "seventeen",
+                                             "eighteen", "nineteen"};
+
+char getSeparator() {
+    return ' ';
+}
+
 int process(std::istream &cin, std::ostream &cout) {
-    std::string destination;
-    double ticketPrice = 0;
-    cin >> destination;
-    while (destination != "End") {
-        cin >> ticketPrice;
-        double collectedMoney = 0;
-        while (collectedMoney < ticketPrice) {
-            double earnedMoney;
-            cin >> earnedMoney;
-            collectedMoney += earnedMoney;
-        }
-        if (collectedMoney >= ticketPrice) {
-            cout << "Going to " << destination << "!" << std::endl;
-        }
-        cin >> destination;
+    int number;
+    cin >> number;
+    if (number == 0) {
+        cout << "zero" << std::endl;
+        return 0;
     }
+    int thousands = number / 1000 % 10;
+    int hundreds = number / 100 % 10;
+    int tens = number / 10 % 10;
+    int singles = number / 1 % 10;
+    if (thousands != 0) {
+        cout << singleDigits[thousands - 1];
+        cout << getSeparator();
+        cout << "thousand";
+        cout << getSeparator();
+    }
+    if (hundreds != 0) {
+        cout << singleDigits[hundreds - 1];
+        cout << getSeparator();
+        cout << "hundred";
+        cout << getSeparator();
+    }
+    if (tens == 1) {
+        cout << toNineteen[singles - 1];
+        return 0;
+    } else if (tens != 0) {
+        cout << tensNumber[tens - 1];
+        cout << getSeparator();
+    }
+
+    if (singles != 0) {
+        cout << singleDigits[singles - 1];
+    }
+    cout << std::endl;
+
     return 0;
 }
 
