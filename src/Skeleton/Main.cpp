0c16d034c5ee0e91ae824f9bb986566fa5dfaffd
Boyan Mihovski <bmihovski@live.com>
2025-06-01 11:14:17 +0300

    task1 completed

--- src/Skeleton/Main.cpp
+++ src/Skeleton/Main.cpp
@@ -1,41 +1,63 @@
-#include <cstddef>
+#include <cstdlib>
 #include <iostream>
-#include <sstream>
-#include <string>
-#include <vector>
+#include <ostream>
+
+double AddedPointsAfterPreviousDay(const double points_previous_day) {
+  double added_points = 0;
+  added_points += points_previous_day * .05;
+  return added_points;
+}
+
+double CalculateDiscountAtTheEnd(const double total_expenses,
+                                 const double total_points,
+                                 const int target_points) {
+  double discount = .0;
+  if (total_points >= target_points) {
+    discount = total_expenses * .25;
+  } else {
+    discount = total_expenses * .1;
+  }
+  return discount;
+}
 
 int main() {
-  int num_elems = 0;
-  int sum_odd = 0;
-  int sum_even = 0;
-  int sum_total = 0;
-  int average = 0;
+  int days = 0;
+  int target_points = 0;
+  int swimmers_count = 0;
+  double hotel_price_per_day_per_swimmer = .0;
+  double participation_fee_per_swimmer = .0;
+  double total_expenses = .0;
+  double total_hotel = .0;
+  double total_participation_fee = .0;
+  double total_points = 0;
 
-  std::cin >> num_elems;
-  std::vector<int> elems;
-  elems.reserve(num_elems);
-  std::string input;
-  std::getline(std::cin >> std::ws, input);
-  std::stringstream ss(input);
-  int val = 0;
-  while (ss >> val) {
-    sum_total += val;
-    elems.push_back(val);
-  }
-  average = sum_total / num_elems;
+  std::cin >> days >> target_points >> swimmers_count >>
+      hotel_price_per_day_per_swimmer >> participation_fee_per_swimmer;
+  total_hotel = days * (hotel_price_per_day_per_swimmer * swimmers_count);
+  total_participation_fee = swimmers_count * participation_fee_per_swimmer;
+  total_expenses = total_hotel + total_participation_fee;
+  double points_previous_day = 0;
 
-  for (size_t start = 0; start < num_elems; ++start) {
-    int cur_elem = elems[start];
-    if (cur_elem > average) {
-      continue;
+  for (int start_day = 1; start_day <= days; ++start_day) {
+    double points_current_day = 0;
+    std::cin >> points_current_day;
+    if (start_day > 1) {
+      points_current_day += AddedPointsAfterPreviousDay(points_previous_day);
     }
-    if (start % 2 == 0) {
-      sum_even += elems[start];
-    } else {
-      sum_odd += elems[start];
-    }
+    total_points += points_current_day;
+    points_previous_day = points_current_day;
   }
-  std::cout << (sum_even * sum_odd) << std::endl;
+  double const discount =
+      CalculateDiscountAtTheEnd(total_expenses, total_points, target_points);
+  total_expenses = std::abs(total_expenses - discount);
+  std::cout.setf(std::ios::fixed);
+  std::cout.precision(2);
+  std::cout << "Money left to pay: " << total_expenses << " BGN." << std::endl;
+  if (total_points >= target_points) {
+    std::cout << "The championship was successful!" << std::endl;
+  } else {
+    std::cout << "The championship was not successful." << std::endl;
+  }
 
   return 0;
 }
