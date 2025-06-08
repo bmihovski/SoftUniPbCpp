45b51f192cf40f6cbb4b6b173bdeb7ad35b2c643
Boyan Mihovski <bmihovski@live.com>
2025-06-08 12:43:28 +0300

    reshena retake 08.06.25

--- src/Skeleton/Main.cpp
+++ src/Skeleton/Main.cpp
@@ -1,61 +1,39 @@
+#include <algorithm>
 #include <iostream>
 #include <iterator>
 #include <sstream>
 #include <string>
 #include <vector>
-
-void PrintTrain(const std::vector<std::string>& train) {
-  std::cout << "|";
-  for (const auto& vagon : train) {
-    std::cout << vagon << "|";
+size_t CountSpecialSymbols(const std::string& sentence) {
+  size_t count = 0;
+  for (const auto ch : sentence) {
+    if (!std::isalnum(ch) && !std::isspace(ch)) {
+      ++count;
+    }
   }
-  std::cout << std::endl;
+  return count;
 }
-
 int main() {
   std::string input;
-  std::vector<std::string> train_start;
-  while (std::getline(std::cin >> std::ws, input) && input != "end") {
-    if (input == "empty") {
-      train_start.push_back(" ");
+  std::vector<size_t> punct_summary;
+  std::getline(std::cin, input);
+  std::stringstream ss(input);
+  while (std::getline(ss, input, '|')) {
+    size_t count = CountSpecialSymbols(input);
+    punct_summary.push_back(count);
+  }
+  std::sort(punct_summary.begin(), punct_summary.end(), std::less<size_t>());
+  size_t current_count = punct_summary.at(0);
+  size_t occur = 0;
+  for (const auto& count : punct_summary) {
+    if (current_count != count) {
+      std::cout << current_count << " symbol sentences: " << occur << std::endl;
+      current_count = count;
+      occur = 1;
     } else {
-      train_start.push_back(input);
+      occur++;
     }
   }
-  if (train_start.empty()) {
-    return 0;
-  }
-  while (true) {
-    std::string command;
-    std::getline(std::cin >> std::ws, command);
-    std::stringstream ss(command);
-
-    std::string command_key;
-    ss >> command_key;
-    if (command_key == "E") {
-      break;
-    }
-    if (command_key == "P") {
-      PrintTrain(train_start);
-    } else if (command_key == "M") {
-      int from = 0;
-      int to = 0;
-      ss >> from >> to;
-      const int old_vagon_position = from - 1;
-      ss >> command_key;
-      const int new_vagon_position = to - 1;
-      const std::string vagon_name = train_start[old_vagon_position];
-      train_start.erase(train_start.begin() + old_vagon_position);
-      train_start.insert(train_start.begin() + new_vagon_position, vagon_name);
-      PrintTrain(train_start);
-    } else if (command_key == "L") {
-      int pos = 0;
-      ss >> pos;
-      const int position_to_remove = pos - 1;
-      train_start.erase(train_start.begin() + position_to_remove);
-      PrintTrain(train_start);
-    }
-  }
-
+  std::cout << current_count << " symbol sentences: " << occur << std::endl;
   return 0;
 }
