96d155faded9eb8e163416e1e479626fc251527a
Boyan Mihovski <bmihovski@live.com>
2025-07-19 12:51:15 +0300

    exam with stack

--- src/Skeleton/Main.cpp
+++ src/Skeleton/Main.cpp
@@ -1,41 +1,104 @@
-#include <cstddef>
+#include <cctype>
 #include <iostream>
+#include <queue>
 #include <sstream>
+#include <stack>
 #include <string>
-#include <vector>
+
+void PrintQueue(std::queue<std::string>& q) {
+  if (q.empty()) {
+    std::cout << "<empty>" << std::endl;
+    return;
+  }
+  while (!q.empty()) {
+    std::cout << q.front() << " ";
+    q.pop();
+  }
+  std::cout << std::endl;
+}
+
+void PrintPeople(std::queue<std::string> queue1, std::queue<std::string> queue2,
+                 std::stack<std::string> chairs) {
+  std::cout << "1: ";
+  PrintQueue(queue1);
+  std::cout << "2: ";
+  PrintQueue(queue2);
+  std::cout << "3: ";
+  if (chairs.empty()) {
+    std::cout << "<empty>" << std::endl;
+    return;
+  }
+  while (!chairs.empty()) {
+    std::cout << chairs.top() << " ";
+    chairs.pop();
+  }
+  std::cout << std::endl;
+}
 
 int main() {
-  int num_elems = 0;
-  int sum_odd = 0;
-  int sum_even = 0;
-  int sum_total = 0;
-  int average = 0;
+  std::queue<std::string> queue1;
+  std::queue<std::string> queue2;
+  std::stack<std::string> chairs;
 
-  std::cin >> num_elems;
-  std::vector<int> elems;
-  elems.reserve(num_elems);
-  std::string input;
-  std::getline(std::cin >> std::ws, input);
-  std::stringstream ss(input);
-  int val = 0;
-  while (ss >> val) {
-    sum_total += val;
-    elems.push_back(val);
-  }
-  average = sum_total / num_elems;
+  std::string line;
 
-  for (size_t start = 0; start < num_elems; ++start) {
-    int cur_elem = elems[start];
-    if (cur_elem > average) {
-      continue;
-    }
-    if (start % 2 == 0) {
-      sum_even += elems[start];
+  while (std::getline(std::cin, line) && line != "5") {
+    std::string name;
+    int number = 0;
+    std::stringstream ss(line);
+    if (!std::isdigit(line[0])) {
+      ss >> name;
+      ss >> number;
+      if (number == 1) {
+        queue1.push(name);
+
+      } else {
+        queue2.push(name);
+      }
     } else {
-      sum_odd += elems[start];
+      ss >> number;
+      const int command_source = number / 10;
+      const int command_dest = number % 10;
+      switch (command_dest) {
+        case 0:
+          if (command_source == 1) {
+            queue1.pop();
+          } else {
+            queue2.pop();
+          }
+          break;
+        case 1: {
+          if (chairs.empty()) {
+            break;
+          }
+          const auto& from_chairs = chairs.top();
+          chairs.pop();
+          queue1.push(from_chairs);
+          break;
+        }
+        case 2: {
+          if (queue1.empty()) {
+            break;
+          }
+          const auto& from_q1 = queue1.front();
+          queue1.pop();
+          queue2.push(from_q1);
+          break;
+        }
+        case 3: {
+          if (queue2.empty()) {
+            break;
+          }
+          const auto& from_q2 = queue2.front();
+          queue2.pop();
+          chairs.push(from_q2);
+          break;
+        }
+        default:
+          PrintPeople(queue1, queue2, chairs);
+      }
     }
   }
-  std::cout << (sum_even * sum_odd) << std::endl;
 
   return 0;
 }
